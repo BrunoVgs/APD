f4f6406497ffaee03611cf69e16768ecbc8d4bf6
BrunoVgs <brunovgs@protonmail.com>
2023-06-09 09:30:36 +0200

    Modification PlayerController

---
+++ .history/src/Controller/PlayerController_20230608152525.php
@@ -0,0 +1,42 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PlayerController extends AbstractController
+{
+    /**
+     * @Route("/admin/joueurs", name="admin_joueurs", methods={"GET"})
+     */
+    public function joueurs(): Response
+    {
+        return $this->render('admin/joueurs/index.html.twig');
+    }
+
+    /**
+    * @Route("/admin/joueurs", name="admin_joueurs", methods={"POST"})
+    */
+    public function add(): Response
+    {
+        return $this->redirectToRoute('admin_joueurs');
+    }
+
+    /**
+    * @Route("/admin/joueurs/{id}", name="admin_joueur_edit", methods={"PUT"})
+    */
+    public function edit(Request $request, $id): Response
+    {
+        return $this->redirectToRoute('admin_joueurs');
+    }
+
+    /**
+    * @Route("/admin/joueurs/{id}", name="admin_joueurs_delete", methods={"DELETE"})
+    */
+    public function delete($id): Response
+    {
+        return $this->redirectToRoute('admin_joueurs');
+    }
+}
